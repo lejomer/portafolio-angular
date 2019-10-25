808d7fed6b49353cf25e9ac553922b1e135b6e6f
Nicolas Rojas Jimenez <jnrojas15@misena.edu.co>
2019-10-24 19:43:18 -0500

    Finalizacion de las animaciones

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,11 @@
+
+export interface InfoPagina {
+  titulo?: string;
+  email?: string;
+  nombre_corto?: string;
+  facebook?: string;
+  twitter?: string;
+  instegram?: string;
+  tublr?: string;
+  Equipo_trabjo?: any[];
+}
